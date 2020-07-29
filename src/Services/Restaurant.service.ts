bf2dd41a44131e938d7d23769262edc7795e372d
Vincius Scoponi <vinicius.scoponi@gmail.com>
2020-07-29 19:09:55 -0300

    Add ErrorHandler

--- src/Services/Restaurant.service.ts
+++ src/Services/Restaurant.service.ts
@@ -1,8 +1,10 @@
+import { ErrorHandler } from './../app/app.errorHandler';
 import { Injectable } from '@angular/core';
 import { IRestaurant } from 'Model/Restaurant.Model';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 import { MeatApi } from 'app/app.api';
 
 @Injectable()
@@ -13,6 +15,8 @@ export class RestaurantService {
     
     getAll(): Observable<IRestaurant[]>
     {
-        return this.http.get(`${MeatApi}/restaurants`).map(response => response.json());
+        return this.http.get(`${MeatApi}/restaurants`)
+        .map(response => response.json())
+        ._catch(ErrorHandler.handleError    );
     }
 }
